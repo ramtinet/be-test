43c51ec5b541550ed9fcc87698ccf84d26198196
ramtinet <ramtinet@kth.se>
2022-06-01 12:08:13 +0200

    added socket.io to backend

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,39 @@
 import express, { Request, Response, NextFunction } from 'express';
 import http from 'http';
 import config from 'config';
-
+import socketio, { Socket } from "socket.io";
 import mailRouter from './mailbox/mailbox.router';
 import log from './services/logging.service';
+import cors from "cors";
+
+const devOptions = {
+  corsOptions: {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) can't handle 204
+  },
+  socketIOOptions: {
+    cors: {
+      origin: "http://localhost:3000"
+    }
+  }
+}
 
 const PORT = config.get<{ port: number }>('app').port;
 
 export default class Server {
   private static expressApp = express();
+  private static http = http.createServer(this.expressApp);
   private static serverInstance: http.Server;
+  public static io = new socketio.Server(this.http, devOptions.socketIOOptions);
+  public static socket: Socket;
 
   public static start(): Promise<void> {
+    this.expressApp.use(cors(devOptions.corsOptions));
     this.addRoutes();
+    this.startSocketIO();
 
     return new Promise((resolve) => {
-      this.serverInstance = this.expressApp.listen(PORT, () => {
+      this.serverInstance = this.http.listen(PORT, () => {
         log.info('Listening on port %s', PORT);
         return resolve();
       });
@@ -26,6 +44,13 @@ export default class Server {
     this.expressApp.use(express.json());
     this.expressApp.use('/mail', mailRouter);
     this.expressApp.use(this.errorMiddleware);
+  }
+
+  private static startSocketIO(){
+    this.io.on('connection', (socket: Socket) => {
+      console.log('We have a new connection!!!');
+      this.socket = socket;
+    });
   }
 
   public static stop(): Promise<void> {
