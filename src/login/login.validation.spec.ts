618a1b8e49d1a1ce9d11ce7b2f9153881583c674
ramtinet <ramtinet@kth.se>
2022-06-02 09:04:59 +0200

    added tests for login

--- src/login/login.validation.spec.ts
+++ src/login/login.validation.spec.ts
@@ -1,13 +1,52 @@
 import { LoginCredentials } from './../types/logincredentials.types';
 import { validateLoginCredentials } from './login.validation';
 
-describe('mailbox-validation', () => {
-  test('does nothing for valid mail', async () => {
+describe('login-validation', () => {
+  it('it should not pass for empty-string', async () => {
+    let passed, shouldNotPass: boolean;
     const loginCredentials: LoginCredentials = {
-        name: "Martin",
+        name: "",
     };
-    validateLoginCredentials(loginCredentials);
-    expect(loginCredentials).toEqual(loginCredentials);
+    try{
+      validateLoginCredentials(loginCredentials);
+      passed = true;
+    } catch (e) {
+      passed = false;
+    }
+    shouldNotPass = !passed;
+    expect(shouldNotPass).toEqual(true);
+  });
+
+  it('it should not pass for blank-string', async () => {
+    let passed, shouldNotPass: boolean;
+    const loginCredentials: LoginCredentials = {
+        name: "   ",
+    };
+    try{
+      validateLoginCredentials(loginCredentials);
+      passed = true;
+    } catch (e) {
+      passed = false;
+    }
+    shouldNotPass = !passed;
+    expect(shouldNotPass).toEqual(true);
+  });
+
+  it('it should not pass for string.length > 20', async () => {
+    let passed, shouldNotPass: boolean;
+    const strSize21 = new Array(20 + 2).join('a');
+    const loginCredentials: LoginCredentials = {
+        name: strSize21,
+    };
+    try{
+      validateLoginCredentials(loginCredentials);
+      passed = true;
+    } catch (e) {
+      passed = false;
+    }
+    shouldNotPass = !passed;
+    expect(strSize21.length).toBeGreaterThan(20);
+    expect(shouldNotPass).toEqual(true);
   });
 
 });
